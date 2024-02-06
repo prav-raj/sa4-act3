f074802ac6b95f03eda52fd4f2b8c25f64e80562
Praveen Varadharaj <pvrdhraj@memphis.edu>
2024-02-06 12:38:25 -0600

    Modified the code to show user how many tries they have left and how many they have leftafter each guess.

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,9 @@
 number = 10
-
+tries = 5
 print("I'm thinking of a number...")
 
 while True:
-    guess = input("What number am I thinking of? Type 'q' to quit: ")
+    guess = input(f"What number am I thinking of? {tries} tries remaining. Type 'q' to quit: ")
 
     if guess.lower() == 'q':
         print(f"The number was {number}. Goodbye!")
@@ -14,4 +14,8 @@ while True:
         break
     else:
         print("Sorry! Try again.")
+        tries -= 1
+    
+    if tries == 0:
+        print(f"You're out of tries! The number was {number}")
 
